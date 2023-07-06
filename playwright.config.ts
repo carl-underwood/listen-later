89d4ec0c0707658443dfc47bccf48b4a3fca7405
Carl Hartshorn <carl.hartshorn@gmail.com>
2023-07-06 09:57:55 +0100

    Tweak Playwright config to run across different devices

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,22 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
-const config: PlaywrightTestConfig = {
+export default defineConfig({
+	forbidOnly: !!process.env.CI,
+	fullyParallel: true,
+	projects: [
+		{ name: 'Android', use: devices['Pixel 5'] },
+		{ name: 'Desktop Chrome', use: devices['Desktop Chrome'] },
+		{ name: 'Desktop Safari', use: devices['Desktop Safari'] },
+		{ name: 'IOS', use: devices['iPhone 13'] }
+	],
+	reporter: [['html', { open: 'never' }]],
+	retries: process.env.CI ? 4 : 0,
+	testDir: 'tests',
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	webServer: {
 		command: 'npm run dev',
 		port: 5000,
 		reuseExistingServer: true
 	},
-	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-	reporter: [['html', { open: 'never' }]]
-};
-
-export default config;
+	workers: process.env.CI ? 2 : undefined
+});
