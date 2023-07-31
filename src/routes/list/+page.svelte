736736c2ab3441af5fbaa4fde1c88a95377297b2
Carl Hartshorn <carl.hartshorn@gmail.com>
2023-07-31 13:35:05 +0100

    Move settings page under list - Also introduce prefers reduced motion support into more animations

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -20,6 +20,9 @@
 			duration: 800
 		});
 	}
+
+	const slideWithPrefersReducedMotion = (node: Element) =>
+		slide(node, { duration: $prefersReducedMotion ? 0 : undefined });
 </script>
 
 {#if $items === undefined}
@@ -38,10 +41,7 @@
 	</a>
 	<div class="my-5">
 		{#if !$items.length}
-			<span
-				class="block text-center mx-4"
-				transition:slide={{ duration: $prefersReducedMotion ? 0 : undefined }}
-			>
+			<span class="block text-center mx-4" transition:slideWithPrefersReducedMotion>
 				Nothing here yet! Use the button above to add an item 👆
 			</span>
 		{/if}
@@ -49,7 +49,7 @@
 			{#each $items as item (item.id)}
 				<div
 					bind:this={accordionItems[item.id]}
-					transition:slide={{ duration: $prefersReducedMotion ? 0 : undefined }}
+					transition:slideWithPrefersReducedMotion
 					class="border-surface-900-50-token ring-4 ring-surface-900-50-token mt-1"
 				>
 					{#if item.service === 'spotify'}
