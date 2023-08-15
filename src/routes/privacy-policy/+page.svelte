c25eeaffaa55d3b87cc5bdd725ae15cdcc3c2ac8
Carl Hartshorn <carl.hartshorn@gmail.com>
2023-08-15 13:05:29 +0100

    Add Privacy Policy

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -1,1 +1,50 @@
-<h1 class="h1">Privacy Policy</h1>
+<div class="max-w-2xl mx-auto p-4">
+	<div class="flex flex-col gap-4">
+		<h1 class="h1">Privacy Policy</h1>
+		<h2 class="h2">About this Policy</h2>
+		<p>
+			This Privacy Policy describes how your personal information is processed by Listen Later ("the
+			application").
+		</p>
+		<p>It applies to usage of Listen Later on any device.</p>
+		<h2 class="h2">Personal Information Collected by Listen Later</h2>
+		<p>
+			Listen Later collects information you provide to create your account and maintain your list.
+			Depending on your chosen sign in method this may include (but is not limited to) your email
+			address and name. This information is collected to maintain your account, allowing sign in on
+			different devices for access to your Listen Later list. This information is not shared with
+			any third party. Listen Later may use your email address to contact you at a later date, for
+			example if the application is being discontinued (as described in the
+			<a href="/terms-of-use" class="underline">Terms of Use</a>).
+		</p>
+		<p>
+			Listen Later also collects information automatically when you use the application. This
+			information includes (but is not limited to) your Internet Protocol address ('IP address'), a
+			unique user identifier, and your device and browser type. This information is collected in
+			order to protect the application from abuse, for example through the use of Google reCAPTCHA,
+			and to support maintenance of the application, for example through application logs.
+		</p>
+		<p>
+			Listen Later does not directly gather analytics or track usage data. The application is hosted
+			using Google Firebase, which maintains some aggregated metrics such as the number of Monthly
+			Active Users for billing purposes.
+		</p>
+		<p>
+			Listen Later uses browser storage technologies (not including cookies) to support your usage
+			of the application. This includes (but is not limited to) capturing your dark/light theme
+			preference, maintaining your sign in session, and supporting usage of Google reCAPTCHA to
+			protect the application from abuse (see above).
+		</p>
+		<h2 class="h2">Data Retention & Erasure</h2>
+		<p>
+			Listen Later retains your information to support your usage of the application, until such a
+			time as you request erasure.
+		</p>
+		<p>
+			Your account may be deleted at any time by visiting the <a
+				href="/list/settings"
+				class="underline">Settings</a
+			> page.
+		</p>
+	</div>
+</div>
