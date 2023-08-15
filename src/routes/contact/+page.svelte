7240c67f6bd3c3db1531e85641359f41a8be0b96
Carl Hartshorn <carl.hartshorn@gmail.com>
2023-08-15 11:35:27 +0100

    Add Contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,25 @@
-<h1 class="h1">Contact</h1>
+<script lang="ts">
+	const fireEmail = () => {
+		const href = 'mailto:hello@listenlater.cloud';
+		const link = document.createElement('a');
+		link.href = href;
+		link.target = '_blank';
+		link.click();
+	};
+</script>
+
+<div class="max-w-2xl mx-auto p-4">
+	<div class="flex flex-col gap-4">
+		<h1 class="h1">Contact</h1>
+		<p>Need to get in touch?</p>
+		<p>
+			For bugs, feature requests, ideas & suggestions, please visit <a
+				href="https://github.com/carl-hartshorn/listen-later/issues"
+				class="underline">Listen Later GitHub Issues</a
+			>.
+		</p>
+		<p>
+			For anything else, <button class="underline" on:click={fireEmail}>drop us an email</button>.
+		</p>
+	</div>
+</div>
